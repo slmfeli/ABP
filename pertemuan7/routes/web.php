6a4ea343ab4228659983045d626f76953d37db23
Felita Salma <pefelisalma@gmail.com>
2026-06-17 10:33:09 +0700

    Hapus semua isi repository

--- pertemuan7/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MahasiswaController;
-
-Route::redirect('/', '/mahasiswa');
-
-Route::resource('mahasiswa', MahasiswaController::class);
